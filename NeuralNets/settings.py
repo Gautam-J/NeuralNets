c5264df147c5be0e6079de27016e3d1e0883dffa
Gautam J <gautam.jayapal@gmail.com>
2020-02-06 17:29:52 +0530

    Update settings.py

--- NeuralNets/settings.py
+++ NeuralNets/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o_1&vro=u0#z-0^41mek(efqr3+to+j5wfal^^!y+gwf%0fb14'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
