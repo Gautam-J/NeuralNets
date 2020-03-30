91139f2b1cb63da51b213633100a69d05be90d8c
Gautam J <gautam.jayapal@gmail.com>
2020-03-30 12:42:05 +0530

    added staticdir

--- NeuralNets/settings.py
+++ NeuralNets/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     'base_templates',
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'base_templates',
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
